9d00c41206e16841afecbc09b4fc7c0ce220230e
albertprofe <albertprofe@gmail.com>
2025-01-22 09:01:16 +0100

    feat: inline css words

--- frontend/pronunciationAppFront/src/Cards.jsx
+++ frontend/pronunciationAppFront/src/Cards.jsx
@@ -19,10 +19,23 @@ export default function WordList() {
 
   return (
     <>
-    <h1>Words List</h1>
+      <h1>Words List</h1>
       {words.map((word) => (
-        <p key={word.id}>
-          {word.id}: <b>{word.word}</b> {word.pronunciation}
+        <p
+          key={word.id}
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            padding: "0 20px",
+          }}
+        >
+          <span style={{ flex: "1", textAlign: "left", color:"grey" }}>{word.id.slice(0,5) + "..."}</span>
+          <span style={{ flex: "1", textAlign: "center" }}>
+            <b>{word.word}</b>
+          </span>
+          <span style={{ flex: "1", textAlign: "right" }}>
+            {word.pronunciation}
+          </span>
         </p>
       ))}
     </>
