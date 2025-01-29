684324f309ae643e0d32f9d2d660a9500cd09fe5
albertprofe <albertprofe@gmail.com>
2025-01-29 07:34:42 +0100

    feat: logout

--- frontend/pronunciationAppFront/src/pages/Home.jsx
+++ frontend/pronunciationAppFront/src/pages/Home.jsx
@@ -1,41 +1,62 @@
 import { useNavigate } from "react-router-dom";
 import Header from "../components/Header.jsx";
-import { Button, Box } from "@mui/material";
+import { Button, Box, Typography } from "@mui/material";
+import { useState } from "react";
 
 export default function Home() {
   const navigate = useNavigate();
+  const [isValidated, setIsValidated] = useState(true);
+  const [username, setUsername] = useState("albertprofe");
 
   const handleRegisterClick = () => {
     navigate("/register");
   };
 
-    const handleLoginClick = () => {
-      navigate("/login");
-    };
+  const handleLoginClick = () => {
+    navigate("/login");
+  };
 
+  const handleLogout = () => {
+    setIsValidated(false);
+  };
 
   return (
     <>
       <h2>Welcome to PronunciationApp</h2>
       <Header />
       <Box sx={{ textAlign: "center", mt: 4 }}>
-        <Box sx={{ mt: 3 }}>
-          <Button
-            variant="contained"
-            onClick={handleLoginClick}
-            color="primary"
-            sx={{ mr: 2 }}
-          >
-            Login
-          </Button>
-          <Button
-            variant="contained"
-            color="secondary"
-            onClick={handleRegisterClick}
-          >
-            Register
-          </Button>
-        </Box>
+        {isValidated ? (
+          <Box>
+            <Typography variant="h5" sx={{ mb: 2 }}>
+              Hello @{username}
+            </Typography>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={handleLogout}
+            >
+              Logout
+            </Button>
+          </Box>
+        ) : (
+          <Box sx={{ mt: 3 }}>
+            <Button
+              variant="contained"
+              onClick={handleLoginClick}
+              color="primary"
+              sx={{ mr: 2 }}
+            >
+              Login
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={handleRegisterClick}
+            >
+              Register
+            </Button>
+          </Box>
+        )}
       </Box>
     </>
   );
